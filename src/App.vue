75ae2834e34dfd99dca6bf00911573842c3ad077
SD QPPMC AVIVALDO RIBEIRO <00668695102@dccpm.local>
2020-05-25 19:00:23 -0300

    Initial project Alura

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ul >
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Animal',
+      fotos: [
+        {
+          titulo: 'tiger',
+          url: 'C:/Users/00668695102/projects/vue-alura/src/assets/elephant.jpg'
+        },
+        {
+          titulo: 'snake',
+          url: __dirname+'/assets/tiger.jpg'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
